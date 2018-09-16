6ec9080bd023180823b3cd8c7caab0e0e8f20055
Eike Dreyer <eikedreyer@gmx.de>
2018-09-16 21:11:44 +0200

    Korrekter Umgang mit Processbuilder zur Deadlock Vermeidung

--- src/main/java/Updater_Main.java
+++ src/main/java/Updater_Main.java
@@ -7,9 +7,9 @@ import main.DashmirrorMain;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -18,16 +18,17 @@ import java.util.Scanner;
  */
 public class Updater_Main extends Application {
 
-    private final String version = "0.1";
+    private final String version = "0.13";
 
     public static int build = 0;
     private int updateCircle = 5;
     private boolean firststart = true;
-    private DashmirrorMain dmain;
-    private Process process;
+    //private Process process;
     private String executPath = "bin/dashmirror/Dashmirror.jar";
     private String os = "windows";
     private Updater updater = new Updater();
+    private Processer processer = new Processer();
+    //private DashmirrorMain dMain;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -39,20 +40,18 @@ public class Updater_Main extends Application {
         Timeline timeline = new Timeline();
         timeline.setCycleCount(Timeline.INDEFINITE);
 
-        dmain = new DashmirrorMain();
-
         JSONParser parser = new JSONParser();
         Object obj = parser.parse(new FileReader("bin/exec.json"));
         JSONObject jsonObject = (JSONObject) obj;
         os = jsonObject.get("os").toString();
         executPath = jsonObject.get("path").toString();
+        String appname = jsonObject.get("appname").toString();
 
-        if(firststart) {
-            System.out.println("starting " + dmain.getAppName());
+        if (firststart) {
+            System.out.println("starting " + appname);
             check(updater.getVersion());
 
-            if(os.equals("windows")) process = startJarOnWindows(executPath);
-            else if(os.equals("linux")) process = startJarOnLinux(executPath);
+            processer.startJar(executPath, os);
 
             firststart = false;
         }
@@ -60,9 +59,8 @@ public class Updater_Main extends Application {
             try {
                 System.out.println("check for updates!");
                 if (check(updater.getVersion())) {
-                    process.destroy();
-                    if(os.equals("windows")) process = startJarOnWindows(executPath);
-                    else if(os.equals("linux")) process = startJarOnLinux(executPath);
+                    processer.destroyProcess();
+                    processer.startJar(executPath, os);
                 } else {
                     System.out.println("no Updates were found, keep going");
                 }
@@ -80,62 +78,17 @@ public class Updater_Main extends Application {
 
         System.out.println("old build main: " + build);
 
-            boolean update = updater.checkForUpdate();
-            if (update) {
-                updater.showChangeLog();
-                return true;
-            } else {
-                System.out.println("altes Programm gestartet");
-                return false;
-            }
+        boolean update = updater.checkForUpdate();
+        if (update) {
+            updater.showChangeLog();
+            return true;
+        } else {
+            System.out.println("altes Programm gestartet");
+            return false;
         }
+    }
 
-    public static void main (String args[]) {
+    public static void main(String args[]) {
         launch();
-    }
-
-    private Process startJarOnWindows(String jarFile) throws InterruptedException,IOException {
-        System.out.println("start on Windows");
-        List alist = new ArrayList<>();
-
-        // add the list of commands to the list
-        alist.add("java");
-        alist.add("-jar");
-        alist.add(jarFile);
-
-        // initialize the processbuilder
-        ProcessBuilder builder = new ProcessBuilder();
-        builder.command(alist);
-        try {
-            // start the process
-            Process p = builder.start();
-            return p;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private Process startJarOnLinux(String jarFile) throws InterruptedException,IOException {
-        System.out.println("start on Linux");
-        List alist = new ArrayList<>();
-
-        // add the list of commands to the list
-        alist.add("java");
-        alist.add("-Djavafx.platform=gtk2");
-        alist.add("-jar");
-        alist.add(jarFile);
-
-        // initialize the processbuilder
-        ProcessBuilder builder = new ProcessBuilder();
-        builder.command(alist);
-        try {
-            // start the process
-            Process p = builder.start();
-            return p;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
